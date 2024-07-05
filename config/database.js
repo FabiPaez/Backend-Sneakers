e48e78dea911db67507413cfe20fdc696302c6ae
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2024-07-05 00:14:53 -0300

    Fix database configuration error

--- config/database.js
+++ config/database.js
@@ -6,11 +6,16 @@ let sequelize;
 if (process.env.DATABASE_URL) {
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth ? params.auth.split(':') : [null, null];
+  const protocol = params.protocol ? params.protocol.replace(':', '') : null;
+
+  if (!protocol) {
+    throw new Error('Dialect needs to be explicitly supplied');
+  }
 
   sequelize = new Sequelize(params.pathname ? params.pathname.substring(1) : null, auth[0], auth[1], {
     host: params.hostname,
     port: params.port,
-    dialect: params.protocol ? params.protocol.replace(':', '') : null,
+    dialect: protocol,
     dialectOptions: {
       ssl: {
         require: true,
@@ -19,6 +24,10 @@ if (process.env.DATABASE_URL) {
     }
   });
 } else {
+  if (!process.env.DB_DIALECT) {
+    throw new Error('Dialect needs to be explicitly supplied');
+  }
+
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
