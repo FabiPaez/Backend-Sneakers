569475b859ab1c895b6b94f1312676f5c762bebc
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2024-07-05 10:45:24 -0300

    migrando a sql lite

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require('sequelize');
-
-// Datos de conexión de JawsDB
-const database = 'mp84y4tzz6dxpj2l';
-const username = 'r2x4kbd8hvya2vw1';
-const password = 'k7pfqbukmx897f4z';
-const host = 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-const port = '3306';
-
-const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  port: port,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+const env = process.env.NODE_ENV || 'development';
+const config = {
+  development: {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
   },
-  logging: false // Si no quieres ver las consultas SQL en la consola
-});
+  test: {
+    dialect: 'sqlite',
+    storage: ':memory:'
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  }
+}[env];
+
+const sequelize = new Sequelize(config);
 
 module.exports = sequelize;
+
+
+
 
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: './credenciales.env' });
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./config/database');
 const User = require('./models/User');
@@ -23,48 +22,33 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
-// // Conectar a la base de datos
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// // Sincronizar la base de datos
-// sequelize.sync()
-//   .then(() => {
-//     console.log('Database synchronized');
-//   })
-//   .catch(err => {
-//     console.error('Error synchronizing the database:', err);
-//   });
-
-// Permitir solicitudes desde tu dominio de GitHub Pages
 const corsOptions = {
-    origin: 'https://fabipaez.github.io',
-    optionsSuccessStatus: 200
+  origin: 'https://fabipaez.github.io',
+  optionsSuccessStatus: 200
 };
-  
+
 app.use(cors(corsOptions));
 
 const startServer = async () => {
-    try {
-        await sequelize.sync({ force: true });
-        console.log('Database synchronized');
-        app.get('/', (req, res) => {
-            res.send('Hello World! This is the root route.');
-          });
-        await initializeProducts(); // Inicializar los productos
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error('Unable to connect to the database:', err);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión a la base de datos establecida con éxito.');
+    
+    await sequelize.sync({ force: true });
+    console.log('Base de datos sincronizada');
+
+    app.get('/', (req, res) => {
+      res.send('Hello World! This is the root route.');
+    });
+
+    // await initializeProducts(); // Inicializar los productos
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('No se pudo conectar a la base de datos:', err);
+  }
 };
 
 startServer();
-
-
