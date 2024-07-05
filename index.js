f2d8769de3ec85c201414979c297f8dbf62f5011
FabiPaez <89799622+FabiPaez@users.noreply.github.com>
2024-07-05 00:38:00 -0300

    Fix Initialize products

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const startServer = async () => {
         app.get('/', (req, res) => {
             res.send('Hello World! This is the root route.');
           });
-        //await initializeProducts(); // Inicializar los productos
+        await initializeProducts(); // Inicializar los productos
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
